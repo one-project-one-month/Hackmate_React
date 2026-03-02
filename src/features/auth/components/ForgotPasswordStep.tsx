64d08456dd9e81da20ddb115073245edfc09ad5d
Zwe Let Yar <letyarzwe@gmail.com>
2026-03-02 14:47:02 +0630

    feat:forgotPasswordStep ui

--- src/features/auth/components/ForgotPasswordStep.tsx
+++ src/features/auth/components/ForgotPasswordStep.tsx
@@ -1,7 +1,79 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 
 const ForgotPasswordStep = () => {
-  return <div>ForgotPasswordStep</div>;
+  const form = useForm({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  return (
+    <div className="w-full bg-transparent">
+      <div className="text-center mb-6">
+        <h1 className="text-3xl font-serif text-zinc-100">
+          Forgot Your Password?
+        </h1>
+        <p className="text-gray-400 text-base mt-1">
+          Enter your email to receive OTP code.
+        </p>
+      </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => {
+            console.log(data);
+          })}
+          className="space-y-4 max-w-xs m-auto"
+        >
+          {/* Email */}
+          {/* real validation rules will be implemented later */}
+          <FormField
+            control={form.control}
+            name="email"
+            rules={{
+              required: "Email is required",
+            }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-gray-300">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your email"
+                    className="bg-transparent border-zinc-200/50 text-gray-200 rounded-lg h-10"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="w-full bg-[#0097b2] hover:bg-[#00869d] text-white rounded-lg h-10 mt-4"
+          >
+            Send
+          </Button>
+        </form>
+      </Form>
+
+      <p className="mt-5 text-center text-sm text-gray-400">
+        Don't have an account?{" "}
+        <a href="#" className="text-cyan-500 hover:text-cyan-400 font-medium">
+          Signup here
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default ForgotPasswordStep;
