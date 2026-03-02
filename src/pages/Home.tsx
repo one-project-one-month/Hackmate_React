13b514124b4957a6b3cf66a0eb7c7079dbd63deb
Min Khant Kyaw <mkhantk102@gmail.com>
2026-03-03 00:33:40 +0630

    feat: add base landing page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main className="min-h-screen w-full bg-radial from-fuchsia-800 to-indigo-900">
+      <div></div>
+    </main>
+  );
+}
+
+export default Home;
