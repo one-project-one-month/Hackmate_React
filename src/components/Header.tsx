ac223fad76f801f93be79a31700bec7c8e901a16
Min Khant Kyaw <mkhantk102@gmail.com>
2026-03-03 00:33:14 +0630

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { useAppDispatch } from "@/hooks/useAppHook";
+import { setIsOpen, setStep } from "@/features/auth/slice";
+
+export default function Header() {
+  const dispatch = useAppDispatch();
+  return (
+    <div className="w-full py-5 px-12 flex justify-between items-center fixed top-0">
+      <h1 className="text-2xl font-bold">logo</h1>
+      <div className="flex gap-5">
+        <Button
+          type="button"
+          className="md:px-6 ring ring-cyan-500 bg-transparent hover:bg-cyan-600 hover:shadow-2xl"
+          onClick={() => {
+            dispatch(setIsOpen(true));
+            dispatch(setStep("login"));
+          }}
+        >
+          Log In
+        </Button>
+        <Button
+          type="button"
+          className="md:px-6 bg-cyan-600 hover:bg-zinc-400 hover:shadow-2xl"
+          onClick={() => {
+            dispatch(setIsOpen(true));
+            dispatch(setStep("signUpMethod"));
+          }}
+        >
+          Sign Up
+        </Button>
+      </div>
+    </div>
+  );
+}
