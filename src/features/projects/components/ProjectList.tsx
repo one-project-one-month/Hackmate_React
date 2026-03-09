8193491cbf251243987379d817c5fbbdebc13bd9
Zwe Let Yar <letyarzwe@gmail.com>
2026-03-10 01:26:16 +0630

    feat:dynamic with mock data

--- src/features/projects/components/ProjectList.tsx
+++ src/features/projects/components/ProjectList.tsx
@@ -1,89 +1,22 @@
 import ProjectListItem from "./ProjectListItem";
+import { mockProjects } from "../mock";
 
 export default function ProjectList() {
   return (
-    <div className="flex flex-col gap-4 w-full h-full items-center bg-white/10 backdrop-blur-lg rounded-xl ">
-      <div>Project Tab component</div>
-      <div>Search Bar component</div>
+    <div className="flex flex-col gap-4 w-full h-full items-center bg-white/10 backdrop-blur-lg rounded-2xl ">
+      <div className="text-white text-lg mt-4">Project Tab component</div>
+      <div className="text-white">Search Bar component</div>
 
       <div className="flex flex-col gap-3 w-full h-115 p-7  overflow-y-auto custom-scrollbar">
-        <div>
-          <ProjectListItem
-            title="Project 1"
-            members={5}
-            image="/images/project1.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 2"
-            members={3}
-            image="/images/project2.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
-        <div>
-          <ProjectListItem
-            title="Project 3"
-            members={7}
-            image="/images/project3.jpg"
-          />
-        </div>
+        {mockProjects.map((project) => (
+          <div key={project.id}>
+            <ProjectListItem
+              title={project.title}
+              members={project.required_roles.length} //this is not member count, just for demo
+              image={project.image_url || "/images/default.jpg"}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
