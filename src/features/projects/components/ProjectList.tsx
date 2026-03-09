09b2331fc91772e5e1eeaacd9fbbde49e3fa4762
Zwe Let Yar <letyarzwe@gmail.com>
2026-03-10 01:01:11 +0630

    feat:add scroll bar

--- src/features/projects/components/ProjectList.tsx
+++ src/features/projects/components/ProjectList.tsx
@@ -2,11 +2,11 @@ import ProjectListItem from "./ProjectListItem";
 
 export default function ProjectList() {
   return (
-    <div className="flex flex-col gap-4 w-full items-center bg-white/10 backdrop-blur-lg rounded-xl">
+    <div className="flex flex-col gap-4 w-full h-full items-center bg-white/10 backdrop-blur-lg rounded-xl ">
       <div>Project Tab component</div>
       <div>Search Bar component</div>
 
-      <div className="flex flex-col gap-3 w-full p-7">
+      <div className="flex flex-col gap-3 w-full h-115 p-7  overflow-y-auto custom-scrollbar">
         <div>
           <ProjectListItem
             title="Project 1"
@@ -19,6 +19,62 @@ export default function ProjectList() {
             title="Project 2"
             members={3}
             image="/images/project2.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
           />
         </div>
         <div>
