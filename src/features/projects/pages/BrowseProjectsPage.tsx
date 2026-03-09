3e5ae90b39dc406a4ffa8288ec56727c6fa68208
Min Khant Kyaw <mkhantk102@gmail.com>
2026-03-09 21:49:43 +0630

    feat: Add layout for project fature

--- src/features/projects/pages/BrowseProjectsPage.tsx
+++ src/features/projects/pages/BrowseProjectsPage.tsx
@@ -1,3 +1,27 @@
-export function BrowseProjectsPage() {
-  return <div>browse project page</div>;
+import ProjectCard from "../components/ProjectCard";
+import SwipeAction from "../components/SwipeAction";
+import SwipeHint from "../components/SwipeHint";
+
+// BrowseProjectsPage.tsx
+export default function BrowseProjectsPage() {
+  return (
+    <div className="min-h-screen p-5 flex flex-col justify-start items-start">
+      {/* Header section */}
+      <div className="">
+        <h1 className="text-5xl font-bold font-serif text-zinc-200 leading-relaxed">
+          Browse Projects
+        </h1>
+        <p className="text-zinc-400">
+          Explore all available projects and quickly find what you are looking
+          for.
+        </p>
+      </div>
+      {/* <ProjectCard /> */}
+      <ProjectCard />
+      {/* <SwipeHint /> */}
+      <SwipeHint />
+      {/* <SwipeActions /> */}
+      <SwipeAction />
+    </div>
+  );
 }
--- src/features/projects/pages/MyProjectsPage.tsx
+++ src/features/projects/pages/MyProjectsPage.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import ProjectList from "../components/ProjectList";
+import ProjectTab from "../components/ProjectTab";
 
+// MyProjectsPage.tsx
 export default function MyProjectsPage() {
-  return <div>MyProjectsPage</div>;
+  return (
+    <div className="min-h-screen p-5 flex flex-col justify-start items-start">
+      {/* Header section */}
+      <div>
+        <h1 className="text-5xl font-serif font-bold text-zinc-200 leading-relaxed">
+          Project Group List
+        </h1>
+        <p className="text-zinc-400">
+          An overview of all project groups you have created and joined for
+          active collaboration.
+        </p>
+      </div>
+
+      {/* <ProjectTabs /> */}
+      <ProjectTab />
+      <ProjectList />
+      {/* <ProjectList /> */}
+    </div>
+  );
 }
