7175c23224b1e1e46321e5b6b7495e46cd1d5015
Zwe Let Yar <letyarzwe@gmail.com>
2026-03-10 00:39:04 +0630

    feat:project List raw ui

--- src/features/projects/components/ProjectList.tsx
+++ src/features/projects/components/ProjectList.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import ProjectListItem from "./ProjectListItem";
 
 export default function ProjectList() {
-  return <div>ProjectList</div>;
+  return (
+    <div className="flex flex-col gap-4 w-full items-center bg-white/10 backdrop-blur-lg rounded-xl">
+      <div>Project Tab component</div>
+      <div>Search Bar component</div>
+
+      <div className="flex flex-col gap-3 w-full p-7">
+        <div>
+          <ProjectListItem
+            title="Project 1"
+            members={5}
+            image="/images/project1.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 2"
+            members={3}
+            image="/images/project2.jpg"
+          />
+        </div>
+        <div>
+          <ProjectListItem
+            title="Project 3"
+            members={7}
+            image="/images/project3.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 // list of projects in projects tab
