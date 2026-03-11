f6d471b17826b19361fb002c015f096e0a531eb2
Zwe Let Yar <letyarzwe@gmail.com>
2026-03-12 02:42:01 +0630

    feat:add project tabs

--- src/features/projects/components/ProjectTab.tsx
+++ src/features/projects/components/ProjectTab.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
 
-export default function ProjectTab() {
-  return <div>ProjectTab</div>;
-}
+const ProjectTabs = () => {
+  const [activeTab, setActiveTab] = useState<"created" | "joined">("created");
 
-// tab to switch between created and joined tabs
+  return (
+    <div className="flex w-full rounded-md bg-slate-600/20   backdrop-blur-md p-2 shadow-lg">
+      <button
+        onClick={() => setActiveTab("created")}
+        className={`
+          flex-1 rounded-md px-6 py-3 text-sm text-white font-medium transition-all duration-200 ease-in-out cursor-pointer 
+          ${
+            activeTab === "created"
+              ? "bg-white/10  shadow-inner shadow-slate-900/30"
+              : ""
+          }
+        `}
+      >
+        Created Projects
+      </button>
+      <button
+        onClick={() => setActiveTab("joined")}
+        className={`
+          flex-1 rounded-md px-6 p-3 text-sm text-white font-medium transition-all duration-200 ease-in-out cursor-pointer
+          ${
+            activeTab === "joined"
+              ? "bg-white/10  shadow-inner shadow-slate-900/30"
+              : ""
+          }
+        `}
+      >
+        Joined Projects
+      </button>
+    </div>
+  );
+};
+
+export default ProjectTabs;
